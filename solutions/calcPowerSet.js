980a0f8f6820fbf7713a11520613194feeb61196
velascoDev <dracontium2@gmail.com>
2017-11-03 12:12:00 -0500

    began powerset problem

---
+++ solutions/calcPowerSet.js
@@ -0,0 +1,12 @@
+/**
+* Calculate the power set of a set.
+* Recursive approach.
+* Time Complexity: 
+* Space Complexity:
+* @param {Object} s. 
+* @returns {Object} Power set of s.
+*/
+
+function calcPowerSet(s) {
+  
+}
